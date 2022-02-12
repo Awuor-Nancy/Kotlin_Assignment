fbac5a1d089e23a74e1d386a32e49b64a166f859
Mundurujoana <mundujoana@gmail.com>
2022-02-12 18:39:19 +0300

    Kotlin First Assignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+fun main() {
+    var fullname :String = "nancyAwuor"
+    println(fullname)
+
+    val studentsAge :Int = 27
+    println (studentsAge)
+
+    val phoneNo :String = "0742507533"
+    println(phoneNo)
+
+    var weightkg : Float = 37.5f
+    val name: String = "kg"
+    print(weightkg)
+    println(name)
+
+    val isKenyan : Boolean = true
+    val isnotKenyan : Boolean = false
+    println(isKenyan)
+    println(isnotKenyan)
+
+
+
+
+
+
+
+
+}
